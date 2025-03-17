865ed1b2ff3b7251094b62cfcfd62d57909349f6
Apratim Dutta <2205274@kiit.ac.in>
2025-03-17 19:40:21 +0530

    Added Frontend

---
+++ repos/react-store/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {HashRouter as Router, Route, Routes} from 'react-router-dom'
+import { Layout } from './components/layout'
+import { Home } from './pages/home'
+import { ProductDetail } from './pages/product-detail'
+import { ProductList } from './pages/product-list'
+import { NotFound } from './pages/not-found'
+import {}
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+
+          </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
